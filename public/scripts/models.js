6b09147f937379fe4ae671ea3098e541d762433b
pipingpusri3b <piping.pusri3b@gmail.com>
2025-11-12 00:38:42 +0700

    Fix case sensitivity in model paths
    Updated model paths to match case sensitivity for URLs.

--- public/scripts/models.js
+++ public/scripts/models.js
@@ -2,10 +2,10 @@ function getUrnForCurrentPage() {
   const path = window.location.pathname.toLowerCase();
 
   const models = {
-    "/construction/rwi.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
-    "/construction/interconnecting.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDIubnd0",
-    "/construction/wtp.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDMubnd0",
-    "/construction/cooling-tower.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDQubnd0",
+    "/Construction/RWI.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
+    "/Construction/interconnecting.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDIubnd0",
+    "/Construction/wtp.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDMubnd0",
+    "/Construction/cooling-tower.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDQubnd0",
   };
 
   return models[path] || null;
