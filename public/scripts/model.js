e625b3c0900a6f48217a5476624d2357b925abff
pipingpusri3b <piping.pusri3b@gmail.com>
2025-11-11 23:27:52 +0700

    Create model.js

---
+++ public/scripts/model.js
@@ -0,0 +1,15 @@
+// scripts/models.js
+export const MODELS = {
+  construction: {
+    rwi: "urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
+    interconnecting: "urn:adsk.objects:os.object:your-bucket-id/interconnecting.svf",
+    wtp: "urn:adsk.objects:os.object:your-bucket-id/wtp.svf",
+    "cooling-tower": "urn:adsk.objects:os.object:your-bucket-id/cooling-tower.svf"
+  },
+  commissioning: {
+    rwi: "urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
+    interconnecting: "urn:adsk.objects:os.object:your-bucket-id/commissioning-interconnecting.svf",
+    wtp: "urn:adsk.objects:os.object:your-bucket-id/commissioning-wtp.svf",
+    "cooling-tower": "urn:adsk.objects:os.object:your-bucket-id/commissioning-cooling-tower.svf"
+  }
+};
