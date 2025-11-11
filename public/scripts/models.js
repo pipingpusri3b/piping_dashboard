28fa569daf28c97e9afe0d092cce0662a4eab8e9
pipingpusri3b <piping.pusri3b@gmail.com>
2025-11-12 00:39:29 +0700

    Update URN format for RWI.html

--- public/scripts/models.js
+++ public/scripts/models.js
@@ -2,7 +2,7 @@ function getUrnForCurrentPage() {
   const path = window.location.pathname.toLowerCase();
 
   const models = {
-    "/Construction/RWI.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
+    "/Construction/RWI.html": "urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
     "/Construction/interconnecting.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDIubnd0",
     "/Construction/wtp.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDMubnd0",
     "/Construction/cooling-tower.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDQubnd0",
