5bda5d68395ef5a487e546ccab725049f075af2f
pipingpusri3b <piping.pusri3b@gmail.com>
2025-11-12 00:49:37 +0700

    Update models.js

--- public/scripts/models.js
+++ public/scripts/models.js
@@ -2,7 +2,7 @@ function getUrnForCurrentPage() {
   const path = window.location.pathname.toLowerCase();
 
   const models = {
-    "/Construction/RWI.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
+    "/construction/rwi.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
     "/construction/interconnecting.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDIubnd0",
     "/construction/wtp.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDMubnd0",
     "/construction/cooling-tower.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDQubnd0",
