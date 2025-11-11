6de2de59a2f34d936fe4515b351ee9d89eeb00f2
pipingpusri3b <piping.pusri3b@gmail.com>
2025-11-12 00:43:18 +0700

    Normalize construction model paths to lowercase
    Updated model paths to be case-insensitive.

--- public/scripts/models.js
+++ public/scripts/models.js
@@ -3,9 +3,9 @@ function getUrnForCurrentPage() {
 
   const models = {
     "/Construction/RWI.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
-    "/Construction/interconnecting.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDIubnd0",
-    "/Construction/wtp.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDMubnd0",
-    "/Construction/cooling-tower.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDQubnd0",
+    "/construction/interconnecting.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDIubnd0",
+    "/construction/wtp.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDMubnd0",
+    "/construction/cooling-tower.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eHh4eHh4eC9Nb2RlbDQubnd0",
   };
 
   return models[path] || null;
