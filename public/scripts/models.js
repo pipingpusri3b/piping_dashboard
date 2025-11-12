dda981b82caf0e71d2bc049583a842303fed8e20
pipingpusri3b <piping.pusri3b@gmail.com>
2025-11-12 11:23:30 +0700

    Update models.js

--- public/scripts/models.js
+++ public/scripts/models.js
@@ -1,10 +1,10 @@
-// Contoh models.js
 function getUrnForCurrentPage() {
   const path = window.location.pathname.toLowerCase();
 
   const models = {
-    "/Construction/rwi.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",  // contoh
-    "/Construction/interconnecting.html": "dXJuOmFkc2sub2JqZWN0czpvcy5ldS1jZW50cmFsLTEuLi4=",
+    "/index.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk", // contoh URN
+    "/construction/rwi.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
+    "/construction/interconnecting.html": "dXJuOmFkc2sub2JqZWN0czpvcy5ldS1jZW50cmFsLTEuLi4=",
     // tambahkan lainnya
   };
 
