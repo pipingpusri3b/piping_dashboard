803fe44d6aa9dec4b7deca9af462b8f4969d59fe
pipingpusri3b <piping.pusri3b@gmail.com>
2025-11-12 11:06:45 +0700

    Update models.js

--- public/scripts/models.js
+++ public/scripts/models.js
@@ -1,20 +1,12 @@
+const models = {
+  "/construction/rwi.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
+  "/construction/rwi": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk",
+  "/construction/wtp.html": "dXJuOmFkc2sub2JqZWN0czpvZ...",
+  "/construction/wtp": "dXJuOmFkc2sub2JqZWN0czpvZ..."
+};
+
 function getUrnForCurrentPage() {
   const path = window.location.pathname.toLowerCase();
-
-  const models = {
-    "/construction/rwi.html": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk.",
-    "/construction/rwi": "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXlwbGFudDNkMjAyNTA5MDYvOTcwMDAubndk", // ✅ clean URL support
-
-    "/construction/interconnecting.html": "dXJuOmFkc2sub2JqZWN0czpv...",
-    "/construction/interconnecting": "dXJuOmFkc2sub2JqZWN0czpv...",
-
-    "/construction/wtp.html": "dXJuOmFkc2sub2JqZWN0czpv...",
-    "/construction/wtp": "dXJuOmFkc2sub2JqZWN0czpv...",
-
-    "/construction/cooling-tower.html": "dXJuOmFkc2sub2JqZWN0czpv...",
-    "/construction/cooling-tower": "dXJuOmFkc2sub2JqZWN0czpv..."
-  };
-
-
-  return models[path] || null;
+  const cleanPath = path.endsWith(".html") ? path : path + ".html";
+  return models[path] || models[cleanPath];
 }
